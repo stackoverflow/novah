48462e79fead1d189ce5c6351884bea6da0d48ab
Islon Scherer <islonscherer@gmail.com>
2021-04-07 17:07:12 +0200

    fixed bug in codegen for lets

--- src/main/kotlin/novah/backend/Codegen.kt
+++ src/main/kotlin/novah/backend/Codegen.kt
@@ -281,8 +281,6 @@ class Codegen(private val ast: Module, private val onGenClass: (String, String, 
                 mv.visitLabel(varStartLabel)
                 ctx.put(e.binder, num, varStartLabel)
 
-                // TODO: check if we need to define the variable before generating the letdef
-                // to allow recursion
                 genExpr(e.body, mv, ctx)
                 mv.visitLabel(varEndLabel)
                 ctx.setEndLabel(e.binder, varEndLabel)
@@ -773,10 +771,10 @@ class Codegen(private val ast: Module, private val onGenClass: (String, String, 
                 }
             }
             is Expr.Let -> {
+                go(exp.bindExpr)
                 for (l in lambdas) {
                     l.ignores += exp.binder
                 }
-                go(exp.bindExpr)
                 go(exp.body)
             }
             is Expr.App -> {
